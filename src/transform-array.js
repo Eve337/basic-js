fe17950c0e0de0f1ab6c7a38b882b06a2c69d4b5
Eve337 <vesunchik1945@gmail.com>
2020-09-27 19:28:25 +0300

    fead: add some solutions

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,43 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+
+module.exports = function transform(arr) {
+  if(!Array.isArray(arr)){
+    throw Error;
+  }
+
+  let deletedIndexEl;
+  let resultArr = [];
+  
+  for (let i = 0; i < arr.length; i++){
+    switch(arr[i]){
+      case "--discard-next" :
+        deletedIndexEl = i + 1; 
+        i++;
+        break;
+
+      case "--discard-prev" :
+        if(resultArr.length && deletedIndexEl !== i - 1){
+          resultArr.pop();
+        }
+      break;
+
+      case "--double-next" :
+        if(i < arr.length - 1){
+          resultArr.push(arr[i+1]);
+        }
+      break;
+
+      case "--double-prev" :
+        if(resultArr.length && deletedIndexEl !== i - 1){
+          resultArr.push(arr[i-1]);
+        }
+        break;
+
+      default:
+      resultArr.push(arr[i]);
+    }
+  }
+  return resultArr;
 };
+
